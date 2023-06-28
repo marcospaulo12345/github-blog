113ed7675a9bed2b14bf423b1e36fcdc1330bbf2
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2023-06-28 20:05:17 -0300

    feat(CardProfile) Buscado informações do usuário do github usando axios

--- src/components/CardProfile/index.tsx
+++ src/components/CardProfile/index.tsx
@@ -1,36 +1,55 @@
 import { ArrowSquareOut, Buildings, GithubLogo, Users } from 'phosphor-react'
 import { CardProfileContainer, CardProfileContent } from './styles'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+interface GithubUser {
+  avatar_url: string
+  bio: string
+  html_url: string
+  name: string
+  login: string
+  followers: number
+  company: string
+}
 
 export function CardProfile() {
+  const [githubUser, setGithubUser] = useState<GithubUser>({} as GithubUser)
+
+  async function getInfoUser() {
+    await axios
+      .get('https://api.github.com/users/marcospaulo12345')
+      .then((response) => {
+        setGithubUser(response.data)
+      })
+  }
+
+  useEffect(() => {
+    getInfoUser()
+  }, [])
+
   return (
     <CardProfileContainer>
-      <img src="https://avatars.githubusercontent.com/u/42808003?v=4" alt="" />
+      <img src={githubUser.avatar_url} alt="" />
       <CardProfileContent>
-        <h1>Marcos Paulo</h1>
-        <p>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </p>
+        <h1>{githubUser.name}</h1>
+        <p>{githubUser.bio}</p>
 
         <div>
           <span>
             <GithubLogo size={18} weight="fill" />
-            marcospaulo12345
+            {githubUser.login}
           </span>
           <span>
             <Buildings size={18} weight="fill" />
             vasp
           </span>
           <span>
-            <Users size={18} weight="fill" />4 seguidores
+            <Users size={18} weight="fill" />
+            {githubUser.followers} sequidores
           </span>
         </div>
-        <a
-          href="https://github.com/marcospaulo12345"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href={githubUser.html_url} target="_blank" rel="noreferrer">
           GITHUB
           <ArrowSquareOut size={18} />
         </a>
