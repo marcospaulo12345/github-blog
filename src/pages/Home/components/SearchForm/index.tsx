c6d0d6b7cc07575513051458a21fb9a20f1b2b94
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2023-06-28 20:43:07 -0300

    feat(Home) Buscado as issues do repo

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,11 +1,15 @@
 import { SearchFormContainer } from './styles'
 
-export function SearchForm() {
+interface SearchFormProps {
+  countIssues: number
+}
+
+export function SearchForm({ countIssues }: SearchFormProps) {
   return (
     <SearchFormContainer action="">
       <header>
         <h2>Publicações</h2>
-        <span>6 publicações</span>
+        <span>{countIssues} publicações</span>
       </header>
       <input type="search" placeholder="Buscar conteúdo" />
     </SearchFormContainer>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,46 @@
+import { useEffect, useState } from 'react'
 import { CardPost } from '../../components/CardPost'
 import { CardProfile } from '../../components/CardProfile'
 import { SearchForm } from './components/SearchForm'
 import { HomeContainer, ListPost } from './styles'
+import axios from 'axios'
+
+interface Issue {
+  title: string
+  body: string
+  updated_at: string
+  number: number
+}
+
+interface AllIssues {
+  total_count: number
+  items: Issue[]
+}
 
 export function Home() {
+  const [issues, setIssues] = useState<AllIssues>({} as AllIssues)
+  const text = 'Teste'
+  const username = 'marcospaulo12345'
+  const repo = 'github-blog'
+
+  async function getIssues() {
+    await axios
+      .get(
+        `https://api.github.com/search/issues?q=${text}%20repo:${username}/${repo}`,
+      )
+      .then((response) => {
+        setIssues(response.data)
+      })
+  }
+
+  useEffect(() => {
+    getIssues()
+  }, [])
+
   return (
     <HomeContainer>
       <CardProfile />
-      <SearchForm />
+      <SearchForm countIssues={issues.total_count} />
 
       <ListPost>
         <CardPost />
